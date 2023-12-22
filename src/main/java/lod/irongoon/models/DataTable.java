e59b14c7c79d11d6ca8d43b6d25199d183d1df3c
Ink230 <justinquinn230@gmail.com>
2023-12-22 03:48:35 -0500

    fix missing access modifier in DataTable

--- src/main/java/lod/irongoon/models/DataTable.java
+++ src/main/java/lod/irongoon/models/DataTable.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class DataTable {
     public List<String[]> data;
 
-    DataTable(List<String[]> data) {
+    public DataTable(List<String[]> data) {
         this.data = data;
     }
 }
