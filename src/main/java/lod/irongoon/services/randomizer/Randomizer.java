3722dbeaf138b1d936211b38f002d642c6543e8c
Ink230 <justinquinn230@gmail.com>
2023-12-26 22:31:21 -0500

    update to switch expression

--- src/main/java/lod/irongoon/services/randomizer/Randomizer.java
+++ src/main/java/lod/irongoon/services/randomizer/Randomizer.java
@@ -3,7 +3,6 @@ package lod.irongoon.services.randomizer;
 import legend.game.modding.events.characters.CharacterStatsEvent;
 import lod.irongoon.config.IrongoonConfig;
 import lod.irongoon.models.DivineFruit;
-import lod.irongoon.data.*;
 
 public class Randomizer {
     private static final Randomizer instance = new Randomizer();
@@ -16,28 +15,22 @@ public class Randomizer {
     private final IrongoonConfig config = IrongoonConfig.getInstance();
 
     public DivineFruit doCharacterStats(CharacterStatsEvent character) {
-        switch(config.totalStatsPerLevel) {
-            case RANDOMIZE_BOUNDS_PER_LEVEL:
-                return characterStatsRandomizer.randomizeWithBounds(character.characterId, character.level);
-            case MAINTAIN_STOCK:
-                return characterStatsRandomizer.randomizeMaintainStock(character.characterId, character.level);
-            case AVERAGE_ALL_CHARACTERS:
-                return characterStatsRandomizer.randomizeAverage(character.characterId, character.level);
-            default:
-                return null;
-        }
+        return switch (config.totalStatsPerLevel) {
+            case RANDOMIZE_BOUNDS_PER_LEVEL ->
+                    characterStatsRandomizer.randomizeWithBounds(character.characterId, character.level);
+            case MAINTAIN_STOCK ->
+                    characterStatsRandomizer.randomizeMaintainStock(character.characterId, character.level);
+            case AVERAGE_ALL_CHARACTERS ->
+                    characterStatsRandomizer.randomizeAverage(character.characterId, character.level);
+        };
     }
 
     public DivineFruit doDragoonStats(CharacterStatsEvent dragoon) {
-        switch(config.totalStatsPerLevel) {
-            case RANDOMIZE_BOUNDS_PER_LEVEL:
-                return dragoonStatsRandomizer.randomizeWithBounds(dragoon.characterId, dragoon.level);
-            case MAINTAIN_STOCK:
-                return dragoonStatsRandomizer.randomizeMaintainStock(dragoon.characterId, dragoon.level);
-            case AVERAGE_ALL_CHARACTERS:
-                return dragoonStatsRandomizer.randomizeAverage(dragoon.characterId, dragoon.level);
-            default:
-                return null;
-        }
+        return switch (config.totalStatsPerLevel) {
+            case RANDOMIZE_BOUNDS_PER_LEVEL ->
+                    dragoonStatsRandomizer.randomizeWithBounds(dragoon.characterId, dragoon.level);
+            case MAINTAIN_STOCK -> dragoonStatsRandomizer.randomizeMaintainStock(dragoon.characterId, dragoon.level);
+            case AVERAGE_ALL_CHARACTERS -> dragoonStatsRandomizer.randomizeAverage(dragoon.characterId, dragoon.level);
+        };
     }
 }
