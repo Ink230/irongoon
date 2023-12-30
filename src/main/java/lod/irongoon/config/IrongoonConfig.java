d809c234bc06e3ba7a0fe09e6cd4d94d13fb742d
Ink230 <justinquinn230@gmail.com>
2023-12-30 11:51:45 -0500

    add config.yaml file and implementations for loading config values

--- src/main/java/lod/irongoon/config/IrongoonConfig.java
+++ src/main/java/lod/irongoon/config/IrongoonConfig.java
@@ -2,29 +2,64 @@ package lod.irongoon.config;
 
 import lod.irongoon.data.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
 public class IrongoonConfig {
     private static final IrongoonConfig INSTANCE = new IrongoonConfig();
     public static IrongoonConfig getInstance() {
         return INSTANCE;
     }
 
-    private IrongoonConfig() {}
-
-    public final String externalDataLoadPath = "./mods/data/US/";
+    public final String externalDataLoadPath = "./mods/irongoon/US/";
     public final String externalDataLoadExtension = ".csv";
-    public final String publicSeed = "7963AC95E13873B6";
-    public final long seed = Long.parseUnsignedLong(publicSeed, 16);
+    public final String externalConfigLoadPath = "./mods/irongoon/config.yaml";
+    public final String publicSeed;
+    public final long seed;
     public final int bodyNumberOfStatsAmount = 4;
     public final int dragoonNumberOfStatsAmount = 4;
-    public final TotalStatsPerLevel bodyTotalStatsPerLevel = TotalStatsPerLevel.RANDOMIZE_BOUNDS_PER_LEVEL;
-    public final TotalStatsPerLevel dragoonTotalStatsPerLevel = TotalStatsPerLevel.RANDOMIZE_BOUNDS_PER_LEVEL;
-    public final TotalStatsMonsters monsterTotalStatsPerLevel = TotalStatsMonsters.RANDOMIZE_BOUNDS;
-    public final int TotalStatsMonstersUpperPercentBound = 150;
-    public final int TotalStatsMonstersLowerPercentBound = 50;
-    public final TotalStatsBounds bodyTotalStatsBounds = TotalStatsBounds.STOCK;
-    public final TotalStatsBounds dragoonStatsBounds = TotalStatsBounds.STOCK;
-    public final TotalStatsDistributionPerLevel bodyTotalStatsDistributionPerLevel = TotalStatsDistributionPerLevel.RANDOM;
-    public final TotalStatsDistributionPerLevel dragoonTotalStatsDistributionPerLevel = TotalStatsDistributionPerLevel.RANDOM;
-    public final HPStatPerLevel hpStatPerLevel = HPStatPerLevel.RANDOM;
-    public final SpeedStatPerLevel speedStatPerLevel = SpeedStatPerLevel.RANDOM;
+    public final TotalStatsPerLevel bodyTotalStatsPerLevel;
+    public final TotalStatsPerLevel dragoonTotalStatsPerLevel;
+    public final TotalStatsMonsters monsterTotalStatsPerLevel;
+    public final int TotalStatsMonstersUpperPercentBound;
+    public final int TotalStatsMonstersLowerPercentBound;
+    public final TotalStatsBounds bodyTotalStatsBounds;
+    public final TotalStatsBounds dragoonStatsBounds;
+    public final TotalStatsDistributionPerLevel bodyTotalStatsDistributionPerLevel;
+    public final TotalStatsDistributionPerLevel dragoonTotalStatsDistributionPerLevel;
+    public final HPStatPerLevel hpStatPerLevel;
+    public final SpeedStatPerLevel speedStatPerLevel;
+
+    private IrongoonConfig() {
+        File configFile = new File(externalConfigLoadPath);
+
+        Map<String, Object> yamlConfig = null;
+        try (InputStream inputStream = new FileInputStream(configFile)) {
+            Yaml yaml = new Yaml();
+            yamlConfig = yaml.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            yamlConfig = Map.of();
+        }
+
+        this.publicSeed = (String) yamlConfig.getOrDefault("publicSeed", "7963AC95E13873B6");
+        this.seed = Long.parseUnsignedLong(publicSeed, 16);
+        this.bodyTotalStatsPerLevel = TotalStatsPerLevel.valueOf((String) yamlConfig.getOrDefault("bodyTotalStatsPerLevel", "RANDOMIZE_BOUNDS_PER_LEVEL"));
+        this.dragoonTotalStatsPerLevel = TotalStatsPerLevel.valueOf((String) yamlConfig.getOrDefault("dragoonTotalStatsPerLevel", "RANDOMIZE_BOUNDS_PER_LEVEL"));
+        this.monsterTotalStatsPerLevel = TotalStatsMonsters.valueOf((String) yamlConfig.getOrDefault("monsterTotalStatsPerLevel", "RANDOMIZE_BOUNDS"));
+        this.TotalStatsMonstersUpperPercentBound = (int) yamlConfig.getOrDefault("TotalStatsMonstersUpperPercentBound", 150);
+        this.TotalStatsMonstersLowerPercentBound = (int) yamlConfig.getOrDefault("TotalStatsMonstersLowerPercentBound", 50);
+        this.bodyTotalStatsBounds = TotalStatsBounds.valueOf((String) yamlConfig.getOrDefault("bodyTotalStatsBounds", "STOCK"));
+        this.dragoonStatsBounds = TotalStatsBounds.valueOf((String) yamlConfig.getOrDefault("dragoonStatsBounds", "STOCK"));
+        this.bodyTotalStatsDistributionPerLevel = TotalStatsDistributionPerLevel.valueOf((String) yamlConfig.getOrDefault("bodyTotalStatsDistributionPerLevel", "RANDOM"));
+        this.dragoonTotalStatsDistributionPerLevel = TotalStatsDistributionPerLevel.valueOf((String) yamlConfig.getOrDefault("dragoonTotalStatsDistributionPerLevel", "RANDOM"));
+        this.hpStatPerLevel = HPStatPerLevel.valueOf((String) yamlConfig.getOrDefault("hpStatPerLevel", "RANDOM"));
+        this.speedStatPerLevel = SpeedStatPerLevel.valueOf((String) yamlConfig.getOrDefault("speedStatPerLevel", "RANDOM"));
+    }
 }
